4bb08ae4cd193deebe71f5dbb51f63f10b23f535
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2017-12-15 13:06:22 +0100

    Make sprop test clearer

--- test-suite/success/sprop.v
+++ test-suite/success/sprop.v
@@ -6,24 +6,32 @@ Definition itt : iUnit := fun A a => a.
 Definition iUnit_irr (P : iUnit -> Type) (x y : iUnit) : P x -> P y
   := fun v => v.
 
-Definition iSquash (A:Type) : SProp := forall P : SProp, (A -> P) -> P.
-Definition isquash A : A -> iSquash A := fun a P f => f a.
-Definition iSquash_rect A (P : iSquash A -> SProp) (H : forall x, P (isquash A x))
-  : forall x : iSquash A, P x := fun x => x (P x) H.
+Definition iSquash (A:Type) : SProp
+  := forall P : SProp, (A -> P) -> P.
+Definition isquash A : A -> iSquash A
+  := fun a P f => f a.
+Definition iSquash_rect A (P : iSquash A -> SProp) (H : forall x : A, P (isquash A x))
+  : forall x : iSquash A, P x
+  := fun x => x (P x) (H : A -> P x).
 
-Inductive sBox (A:SProp) : Prop := sbox : A -> sBox A.
+Fail Check (fun A : SProp => A : Type).
+
+Inductive sBox (A:SProp) : Prop
+  := sbox : A -> sBox A.
 
 Definition uBox := sBox iUnit.
 
 Definition sBox_irr A (x y : sBox A) : x = y.
 Proof.
+  Fail reflexivity.
   destruct x as [x], y as [y].
   reflexivity.
 Defined.
 
 Set Primitive Projections.
 (* Primitive record with all fields in SProp has the eta property of SProp so must be SProp. *)
-Record rBox (A:SProp) : Prop := rmkbox { relem : A }.
+Record rBox (A:SProp) : Prop
+  := rmkbox { relem : A }.
 
 (* Check that it doesn't have eta *)
 Fail Check (fun (A : SProp) (x : rBox A) => eq_refl : x = @rmkbox _ (@relem _ x)).
@@ -64,16 +72,25 @@ Fail Definition pb_which b (w:which_pb b) : bool
      | is_pf => false
      end.
 
-Inductive sNZ : nat -> SProp := snz : forall n, sNZ (S n).
+Inductive sNZ : nat -> SProp
+  := snz : forall n, sNZ (S n).
 
 Definition sPred (n:nat) (s:sNZ n) : nat :=
   match s with snz k => k end.
 
-Definition spred1 n (s : sNZ (S n)) : sPred _ s = sPred _ (snz n) := eq_refl.
-Definition spred2 n (s : sNZ (S n)) : sPred _ (snz n) = n := eq_refl.
-Definition spred3 n (s:sNZ _) : sPred _ s = n := eq_trans (spred1 n s) (spred2 n s).
+Definition spred1 n (s : sNZ (S n))
+  : sPred (S n) s = sPred (S n) (snz n)
+  := eq_refl.
+Definition spred2 n (s : sNZ (S n))
+  : sPred (S n) (snz n) = n
+  := eq_refl.
+Definition spred3 n (s:sNZ (S n))
+  : sPred (S n) s = n
+  := eq_trans (spred1 n s) (spred2 n s).
 
-Definition sPred_S n (s:sNZ (S n)) : sPred _ s = n := eq_refl.
+Definition sPred_S n (s:sNZ (S n))
+  : sPred (S n) s = n
+  := eq_refl.
 
 Inductive Ispair (A:Type) (B:A -> Type) : sigT B -> SProp :=
   ispair : forall x y, Ispair A B (existT B x y).
@@ -82,9 +99,6 @@ Definition p1 A B p (i : Ispair A B p) : A :=
   match i with ispair _ _ x _ => x end.
 
 (* TODO
-Definition spred3' n (s:sNZ _) : sPred n s = n := eq_refl.
-
-
 Inductive sprod (A B : SProp) : SProp := spair : A -> B -> sprod A B.
 
 pretyping of matches landing in SProp
