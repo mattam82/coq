76a908c682308fd0f5ad9ab46375921051211493
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2019-06-01 12:25:44 +0200

    Overlay for PR 9918

---
+++ dev/ci/user-overlays/9918-mattam82-template-poly-fix.sh
@@ -0,0 +1,12 @@
+if [ "$CI_PULL_REQUEST" = "9918" ] || [ "$CI_BRANCH" = "fix-template" ]; then
+
+    vst_CI_REF=pr9918-overlay
+    vst_CI_GITURL=https://github.com/mattam82/vst
+
+    bedrock2_CI_REF=pr9918-overlay
+    bedrock2_CI_GITURL=https://github.com/mattam82/bedrock2
+
+#    paramcoq_CI_REF=pr9918-overlay
+#    paramcoq_CI_GITURL=https://github.com/mattam82/paramcoq
+
+fi
