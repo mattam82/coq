fe4da952a4176cce4872eb19d987b6e58f1f0742
Matthieu Sozeau <mattam@mattam.org>
2021-04-19 23:35:29 +0200

    Fix ci-hott script after upstream changes

--- dev/ci/ci-hott.sh
+++ dev/ci/ci-hott.sh
@@ -5,6 +5,5 @@ ci_dir="$(dirname "$0")"
 
 git_download hott
 
-( cd "${CI_BUILD_DIR}/hott" && ./autogen.sh -skip-submodules && ./configure \
-      && sed -i.bak 's/\(HOQC =.*\)/\1 -native-compiler no/' Makefile \
+( cd "${CI_BUILD_DIR}/hott" && coq_makefile -f _CoqProject -o Makefile
       && make && make validate )
