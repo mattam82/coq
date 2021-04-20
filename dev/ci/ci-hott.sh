9062001bc091273eaa46fff342f5651e8901decc
Matthieu Sozeau <mattam@mattam.org>
2021-04-20 09:09:28 +0200

    Update hott ci instructions (thanks to input by Jason Gross)

--- dev/ci/ci-hott.sh
+++ dev/ci/ci-hott.sh
@@ -5,5 +5,5 @@ ci_dir="$(dirname "$0")"
 
 git_download hott
 
-( cd "${CI_BUILD_DIR}/hott" && coq_makefile -f _CoqProject -o Makefile
+( cd "${CI_BUILD_DIR}/hott" && coq_makefile -f _CoqProject -o Makefile -arg -native-compiler -arg no \
       && make && make validate )
