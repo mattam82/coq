e9aeae3bbb8a016001aa4362dce2376b330d114a
Kenji Maillard <kenji@maillard.blue>
2024-07-03 18:22:57 +0200

    register lemmas for rewrite

--- theories/Init/Types/Equality.v
+++ theories/Init/Types/Equality.v
@@ -20,6 +20,13 @@ Notation "x = y :> A" := (@eq A x y) : type_scope.
 Arguments eq {A} x _.
 Arguments eq_refl {A x} , [A] x.
 
+(* Specialization of equality to a single sort *)
+Definition eqdiag@{s|l|} {A : Type@{s|l}} := eq@{s s| l} (A:=A).
+
+Notation "x ≡ y" := (eqdiag x y) (at level 60) : type_scope.
+Notation "x ≡ y :> A" := (@eqdiag A x y) (at level 60) : type_scope.
+
+
 Scheme eq_poly := Induction for eq Sort Poly.
 
 Definition eq_ind@{s | u|} [A] [x] P := @eq_poly@{s Prop|u Set} A x (fun a _ => P a).
@@ -69,8 +76,7 @@ Section GroupoidOperations.
   Definition tr@{b|} {B : Type@{sa|b}} (e : @eq@{_ sa|max(a+1,b+1)} Type@{sa|max(a,b)} A B) : A -> B :=
     match e in @eq _ _ B return A -> B with | eq_refl _ => fun x => x end.
 
-  Sort sb.
-  Definition ap@{b|} {B : Type@{sb|b}} (f : A -> B) {x y : A} (e : x = y) : f x = f y :=
+  Definition ap@{sb|b|} {B : Type@{sb|b}} (f : A -> B) {x y : A} (e : x = y) : f x = f y :=
     match e with | eq_refl _ => eq_refl _ end.
 
 End GroupoidOperations.
@@ -84,3 +90,48 @@ Register eq_poly as core.eq.rect.
 Register eq_sym as core.eq.sym.
 Register eq_trans as core.eq.trans.
 Register congr as core.eq.congr.
+
+(* TODO: Register ! *)
+Definition eq_poly_r@{α β|u v|} (A:Type@{α|u}) (x:A) (P:A -> Type@{β|v}) :
+  P x -> forall y:A, y = x :> A -> P y :=
+  fun px y e =>
+    match e in _ = x return P x -> P y with
+    | eq_refl => fun py => py
+    end px.
+
+
+Definition eq_rect_r@{α|u v|} (A:Type@{α|u}) (x:A) (P:A -> Type@{v}) :
+  P x -> forall y:A, y = x -> P y :=
+  fun px y e =>
+  match e in _ = x return P x -> P y with
+  | eq_refl => fun py => py
+  end px.
+
+Register eq_rect_r as core.eq.rect_r.
+
+Definition eq_rec_r@{α|u|} (A:Type@{α|u}) (x:A) (P:A -> Set) :
+  P x -> forall y:A, y = x -> P y :=
+  fun px y e =>
+  match e in _ = x return P x -> P y with
+  | eq_refl => fun py => py
+  end px.
+
+Register eq_rec_r as core.eq.rec_r.
+
+Definition eq_ind_r@{α|u|} (A:Type@{α|u}) (x:A) (P:A -> Prop) :
+  P x -> forall y:A, y = x -> P y :=
+  fun px y e =>
+    match e in _ = x return P x -> P y with
+    | eq_refl => fun py => py
+    end px.
+
+Register eq_ind_r as core.eq.ind_r.
+
+Definition eq_sind_r@{α|u|} (A:Type@{α|u}) (x:A) (P:A -> SProp) :
+  P x -> forall y:A, y = x -> P y :=
+  fun px y e =>
+  match e in _ = x return P x -> P y with
+  | eq_refl => fun py => py
+  end px.
+
+Register eq_sind_r as core.eq.sind_r.
