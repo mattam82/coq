7b81caff0fce429190445362fe53b604f40b3fb7
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-11-25 15:18:28 +0100

    Fixes in test-suite

--- test-suite/success/polymorphism.v
+++ test-suite/success/polymorphism.v
@@ -392,7 +392,7 @@ Module Anonymous.
     anonalg@{k j}.
   (* i <= j < k *)
   Definition collapsethemiddle := inthemiddle@{i _ j}.
-  Check collapsethemiddle@{_ _ _}.
+  Check collapsethemiddle@{_ _}.
 
 End Anonymous.
 
