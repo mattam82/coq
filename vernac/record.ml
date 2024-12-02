8e0d529fb3a98afac53b5f5fedf1914b961e6bdd
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-12-02 13:25:16 +0100

    Various fixes

--- vernac/record.ml
+++ vernac/record.ml
@@ -539,7 +539,7 @@ let declare_proj_coercion_instance ~flags ref from =
     Classes.declare_instance ~warn:true env sigma (Some info) local ref
   end
 
-let make_projection_variances i variances =
+let _make_projection_variances i variances =
   match variances with
   | None -> None
   | Some v ->
@@ -600,8 +600,8 @@ let build_named_proj ~primitive ~flags ~univs ~uinstance ~kind env paramdecls
     UState.{ universes_entry_universes = UState.Monomorphic_entry Univ.ContextSet.empty;
       universes_entry_binders = snd univs }
   | Entries.Polymorphic_entry (uctx, variances) ->
-    let variances = make_projection_variances (Context.Rel.nhyps paramdecls) variances in
-    UState.{ universes_entry_universes = UState.Polymorphic_entry (uctx, variances);
+    (* let variances = make_projection_variances (Context.Rel.nhyps paramdecls) variances in *)
+    UState.{ universes_entry_universes = UState.Polymorphic_entry (uctx, None);
       universes_entry_binders = snd univs }
   in
   let entry = Declare.definition_entry ~univs ~types:projtyp proj in
