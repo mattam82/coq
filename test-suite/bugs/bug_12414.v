4e766cd2ea2cd35f2a26afebf4ae6a64531ab9cb
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-01-17 09:50:52 +0100

    Adapt bug_12414 to sort poly

--- test-suite/bugs/bug_12414.v
+++ test-suite/bugs/bug_12414.v
@@ -1,13 +1,15 @@
 Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 Set Printing Universes.
-Inductive list {T} : Type := | cons (t : T) : list -> list. (* who needs nil anyway? *)
+Inductive list@{s|l|} {T : Type@{s|l}} : Type@{s | l} := | cons (t : T) : list -> list. (* who needs nil anyway? *)
 Arguments list : clear implicits.
 Section map.
-  Context {A B : Type}.
+  Sort s.
+  Context {A B : Type@{s|_}}.
   Fixpoint map (f: A -> B) (l : list A) : list B :=
   let '(cons t l) := l in cons (f t) (map f l).
-End map. About map@{_ _}.
+End map.
+Check map@{Type|_ _}.
 (* Two universes, as expected. *)
 
 Definition map_Set@{} {A B : Set} := @map A B.
